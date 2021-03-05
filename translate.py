5eea76899189d0917fc46f1b1974d77b90d6f0d7
Yu Sun <yzs0104@auburn.edu>
2021-03-05 17:06:21 -0600

    the last one filed...

--- translate.py
+++ translate.py
@@ -12,23 +12,42 @@ def translate_sequence(rna_sequence, genetic_code):
 
     If `rna_sequence` is less than 3 bases long, or starts with a stop codon,
     an empty string is returned.
-
-    Parameters
+Parameters
     ----------
     rna_sequence : str
         A string representing an RNA sequence (upper or lower-case).
-
     genetic_code : dict
         A dictionary mapping all 64 codons (strings of three RNA bases) to
         amino acids (string of single-letter amino acid abbreviation). Stop
         codons should be represented with asterisks ('*').
-
     Returns
     -------
     str
         A string of the translated amino acids.
     """
-    pass
+    resultseq = ''
+    if len(rna_sequence) >= 3:
+        for i in range(0, len(rna_sequence), 3):
+            codon = rna_sequence[i:i+3]
+            codon = codon.upper()
+            if len(codon) == 3:
+                if codon == 'UAA' or codon == 'UGA' or codon == 'UAG':
+                    break
+                else:
+                    resultseq = resultseq + genetic_code[codon]
+            elif len(codon) < 3:
+                break 
+    
+    translate = ''
+    if len(resultseq) >= 1:
+        for i in resultseq:
+            if i != '*':
+                translate = translate + i
+            else:
+                break
+    
+    return translate
+    pass  
 
 def get_all_translations(rna_sequence, genetic_code):
     """Get a list of all amino acid sequences encoded by an RNA sequence.
@@ -44,22 +63,6 @@ def get_all_translations(rna_sequence, genetic_code):
 
     If no amino acids can be translated from `rna_sequence`, an empty list is
     returned.
-
-    Parameters
-    ----------
-    rna_sequence : str
-        A string representing an RNA sequence (upper or lower-case).
-
-    genetic_code : dict
-        A dictionary mapping all 64 codons (strings of three RNA bases) to
-        amino acids (string of single-letter amino acid abbreviation). Stop
-        codons should be represented with asterisks ('*').
-
-    Returns
-    -------
-    list
-        A list of strings; each string is an sequence of amino acids encoded by
-        `rna_sequence`.
     """
     pass
 
@@ -69,41 +72,45 @@ def get_reverse(sequence):
     Returns a string with `sequence` in the reverse order.
 
     If `sequence` is empty, an empty string is returned.
-
-    Examples
-    --------
-    >>> get_reverse('AUGC')
-    'CGUA'
     """
+    if sequence:
+        seq = sequence.upper()
+        rev_seq = seq[::-1]
+        return rev_seq
+    else:
+        return ''
     pass
 
 def get_complement(sequence):
-    """Get the complement of a `sequence` of nucleotides.
+    """Get the complement of `sequence`.
 
     Returns a string with the complementary sequence of `sequence`.
 
     If `sequence` is empty, an empty string is returned.
-
-    Examples
-    --------
-    >>> get_complement('AUGC')
-    'UACG'
     """
+    if sequence:
+        seq = list(sequence.upper())
+        comp = {'C':'G', 'G':'C', 'U':'A', 'A':'U'}
+        seq=[comp[base] for base in seq]
+        return ''.join(seq)
+    else:
+        return ''
     pass
 
 def reverse_and_complement(sequence):
-    """Get the reversed and complemented form of a `sequence` of nucleotides.
+    """Get the reversed and complemented form of `sequence`.
 
     Returns a string that is the reversed and complemented sequence
     of `sequence`.
 
     If `sequence` is empty, an empty string is returned.
-
-    Examples
-    --------
-    >>> reverse_and_complement('AUGC')
-    'GCAU'
     """
+    if sequence:
+        re_seq = get_reverse(sequence)
+        rc_seq = get_complement(re_seq)
+        return rc_seq
+    else:
+        return''
     pass
 
 def get_longest_peptide(rna_sequence, genetic_code):
@@ -116,22 +123,6 @@ def get_longest_peptide(rna_sequence, genetic_code):
 
     If no amino acids can be translated from `rna_sequence` nor its reverse and
     complement, an empty string is returned.
-
-    Parameters
-    ----------
-    rna_sequence : str
-        A string representing an RNA sequence (upper or lower-case).
-
-    genetic_code : dict
-        A dictionary mapping all 64 codons (strings of three RNA bases) to
-        amino acids (string of single-letter amino acid abbreviation). Stop
-        codons should be represented with asterisks ('*').
-
-    Returns
-    -------
-    str
-        A string of the longest sequence of amino acids encoded by
-        `rna_sequence`.
     """
     pass
 
